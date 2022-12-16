0f647f2b24c96f11f259def5872f1c7064b2d489
Sporesong <114929415+Sporesong@users.noreply.github.com>
2022-12-16 14:59:58 +0100

    added icons and styling for making tasks

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-let tasks = [];
+/*let tasks = [];
 console.log("hej");
 function addTask() {
-	let text = document.querySelector(".addToDo").value;
+	let text = document.querySelector("#addToDo").value;
 	let ul = document.querySelector("ul");
 	const task = document.createElement("li");
 	task.innerHTML = `<span><i class="fa fa-trash"></i></span>${text}`;
@@ -9,9 +9,9 @@ function addTask() {
 	ul.appendChild(task);
 	tasks.push(task);
 }
-document.querySelector(".addToDo").addEventListener("keyup", function (event) {
+document.querySelector("#addToDo").addEventListener("keyup", function (event) {
 	event.preventDefault();
 	if (event.keyCode === 13) {
 		addTask();
 	}
-});
+});*/

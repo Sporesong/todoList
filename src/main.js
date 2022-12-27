fe9f7118399991a3a3c8c4f48a3ed5b3906e4928
Sporesong <114929415+Sporesong@users.noreply.github.com>
2022-12-27 17:52:01 +0100

    added render task functionality

--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
-import { doc } from "prettier";
 import "./style/style.scss";
 //ladda upp sparade tasks från local storage
 let tasks = [];
-//window.onload = loadTasks;
-/*function loadTasks() {
-;Array.from(JSON.parse(localStorage.getItem("tasks")));
+window.onload = loadTasks;
+function loadTasks() {//ska bli renderAllTasks
+	tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+	console.log(tasks);
+	console.log(tasks[0]);
+
 }
 //Skapa uppgiftselement
-/*function renderTask(task) {
+function renderNewTask(task) {
 	const list = document.querySelector(".toDoList");
 	const listItem = document.createElement("li");
-	const deadlineInput = document.querySelector("#datePicker");
-	const deadline = deadlineInput.value;
+	listItem.setAttribute("id",task.id);
 	listItem.innerHTML = `
-	<input type="checkbox" onclick="taskComplete(this)" class="check" ${task.completed ? "checked" : ""}>
-    <span>${task.text} class="task ${task.completed ? "completed" : ""}"</span>
-    <button class="deleteTask" onclick="removeTask(this)>
-	<span class="material-symbols-outlined">delete</span>
-	</button>
-  `;
-	list.append(listItem);
-}*/
-
+	<label for="check">	
+	<input type="checkbox" id="check${task.id}" class="check" name="check" value="yes">
+	<span class="taskText">${task.text} </span><br>
+	<span class="taskDeadline"> ${new Date(task.deadline).toDateString()}</span>
+		<button class="deleteTask">
+	</label>
+		<span class="material-symbols-outlined">delete</span>
+		</button>
+	  `;
+	list.prepend(listItem);
+}
+//onclick="taskComplete(this)" ${task.completed ? "checked" : ""
 //spara uppgift när man trycker på lägg till knappen
 const addToDoButton = document.querySelector(".addToDoButton");
 addToDoButton.addEventListener("click", addTask);
@@ -33,9 +37,8 @@ function addTask(event) {
 	const deadline = deadlineInput.value ? new Date(deadlineInput.value).getTime() : "";
 	const categoryPicker =  document.querySelector("#categoryPicker");
 	const category = categoryPicker.value ?? null; 
-	const task = {id: Date.now(), text: text, deadline: deadline, category: category};
-	console.log(task);
-	tasks.push(task);
+	const task = {id: Date.now(), text: text, deadline: deadline, category: category, completed:false};
+	tasks.unshift(task);
 	localStorage.setItem("tasks", JSON.stringify(tasks));
 	if (text) {
 		input.value = "";
@@ -44,6 +47,14 @@ function addTask(event) {
 	else { 
 		document.getElementsByName("addToDo")[0].placeholder="Fyll i en uppgift"; 
 		//påminnelse att skriva något om man försöker lägga till tom uppgift
-	}//TODO: gör så att kategori återställs till tomt värde efter varje submit-klick, samma för datum.
-}
+	}
 
+	renderNewTask(task);
+	const checkBox = document.querySelector(`#check${task.id}`);
+	console.log(checkBox);
+	//checkBox.addEventListener("change", );
+	//TODO: gör så att kategori återställs till tomt värde efter varje submit-klick, samma för datum.
+}
+function completeTask() {
+
+}

fcd30632c4d3f9ab545b7ef8ba5910f6dfdd2537
Sporesong <114929415+Sporesong@users.noreply.github.com>
2022-12-28 16:38:39 +0100

    Added renderTasks function & fixed complete task

--- src/main.js
+++ src/main.js
@@ -9,11 +9,32 @@ let categoryIcons = {
 //ladda upp sparade tasks från local storage
 let tasks = [];
 window.onload = loadTasks;
-function loadTasks() {//ska bli renderAllTasks
-	tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
-	console.log(tasks);
-	console.log(tasks[0]);
-
+function loadTasks() {
+	const taskString = localStorage.getItem("tasks");
+	tasks = taskString === null ? [] : Array.from(JSON.parse(taskString));
+	renderTasks();
+}
+function renderTasks() {
+	const toDoList = document.querySelector(".toDoList");
+	toDoList.innerHTML = "";
+	tasks.forEach (function createStoredElement(task) {
+		const listItem = document.createElement("li");
+		listItem.setAttribute("id",task.id);
+		listItem.innerHTML +=  `
+		<label for="check">	
+		<input type="checkbox" id="check${task.id}" class="check" name="check" ${task.completed === true ? "checked" : ""}>
+		<span class="material-symbols-outlined">${categoryIcons[task.category]}</span>
+		<span class="taskText ${task.completed === true ? "complete" : ""}">${task.text} </span>
+		<span class="taskDeadline"> ${task.deadline ? new Date(task.deadline).toDateString() : ""}</span><br>
+			<button class="deleteTask">
+		</label>
+			<span class="material-symbols-outlined">delete</span>
+			</button>
+		  `;
+		toDoList.append(listItem);
+		const checkBox = document.querySelector(`#check${task.id}`);
+		checkBox.addEventListener("change",toggleCompleteTask);
+	});
 }
 //Skapa uppgiftselement
 function renderNewTask(task) {
@@ -23,7 +44,7 @@ function renderNewTask(task) {
 	listItem.classList.add("listItem");
 	listItem.innerHTML = `
 	<label for="check">	
-	<input type="checkbox" id="check${task.id}" class="check" name="check" value="yes">
+	<input type="checkbox" id="check${task.id}" class="check" name="check">
 	<span class="material-symbols-outlined">${categoryIcons[task.category]}</span>
 	<span class="taskText">${task.text} </span>
 	<span class="taskDeadline"> ${task.deadline ? new Date(task.deadline).toDateString() : ""}</span><br>
@@ -60,26 +81,24 @@ function addTask(event) {
 	renderNewTask(task);
 	//lägg till id på checkbox
 	const checkBox = document.querySelector(`#check${task.id}`);
-	console.log(checkBox);
 	checkBox.addEventListener("change",toggleCompleteTask);
 	//TODO: gör renderTasks funktion och anropa den på rätt ställen i koden, gör så att info hämtas ur localStorage ist. för tasks
+	//töm ul i inner html i början av rendertasks 
 
 }
-//funktion för att checka i task och stryka över texten samt flytta dem sist i arrayen
+//funktion för att checka i task och flytta dem sist i arrayen
 function toggleCompleteTask(event) {
+	const index = tasks.findIndex(function compareId(task){
+		return event.target.parentElement.parentElement.id === task.id; //checkar li-id
+	});
 	if (event.target.checked) {
-		event.target.parentNode.querySelector(".taskText").classList.add("complete");
-		const index = tasks.findIndex(function compareId(task){
-			return event.target.parentElement.parentElement.id === task.id; //checkar li-id
-		});
-		const task= tasks.splice(index, 1)[0];
+		const task = tasks.splice(index, 1)[0];
+		task.completed = true;
 		tasks.push(task);
-		console.log(tasks);
 	}
 	else {
-		event.target.parentNode.querySelector(".taskText").classList.remove("complete");
-
+		tasks[index].completed = false;
 	}
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	renderTasks();
 }
-
-
